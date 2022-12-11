a1693afd1ffc1dbef702c986157faafd77923e18
VA-Freelancer <vproger@vk.com>
2022-12-12 01:03:04 +0400

    add blade temp androute, fix naming, add form

--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -1,20 +1,18 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
-        <i class="fas fa-solid fa-crown"></i>
-        <span class="brand-text font-weight-light">VProger</span>
+
+    <a href="index3.html" class="brand-link d-flex align-items-center " style="gap: 10px;">
+        <i class="fas fa-solid fa-robot"></i>
+        <span class="brand-text font-weight-light text-uppercase font-weight-bold">VProger</span>
     </a>
 
     <!-- Sidebar -->
     <div class="sidebar">
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-
-
-
                 <li class="nav-item">
-                    <a href="#" class="nav-link">
+                    <a href="{{ route('admin.category.index') }}" class="nav-link">
 
                         <i class="nav-icon fa-solid fas fa-list"></i>
                         <p>
