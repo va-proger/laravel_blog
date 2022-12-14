f10ddd0b3a83b70ccec3202ab39b3ba6aa9e4ec8
VA-Freelancer <vproger@vk.com>
2022-12-15 01:36:59 +0400

    add edit, fix controller edit, fix class service, fix blade show, fox semelink storage

--- resources/views/admin/post/edit.blade.php
+++ resources/views/admin/post/edit.blade.php
@@ -29,14 +29,80 @@
             <!-- Small boxes (Stat box) -->
             <div class="row">
                 <div class="col-12">
-                    <form action="{{ route('admin.post.update', $post->id) }}" method="POST" class="w-25 ">
+                    <form action="{{ route('admin.post.update', $post->id) }}" method="POST" class=" " enctype="multipart/form-data">
                         @csrf
                         @method('PATCH')
-                        <div class="form-group">
-                            <input type="text" name="title" class="form-control" placeholder="Название категории" value="{{ $post->title }}">
+                        <div class="form-group w-25">
+                            <input type="text" name="title" class="form-control" placeholder="Название категории" value="{{ $post->title}}">
                             @error('title')
-                                <div class="text-danger">Это поле необходимо для заполнения</div>
+                            <div class="text-danger">Это поле необходимо для заполнения</div>
                             @enderror
+                        </div>
+                        <div class="form-group">
+                            <textarea id="summernote" name="content">{{ $post->content }}</textarea>
+                            @error('content')
+                            <div class="text-danger">Это поле необходимо для заполнения</div>
+                            @enderror
+                        </div>
+                        <div class="form-group w-25">
+                            <label for="exampleInputFile">Добавить превью</label>
+                            <div class="w- mb-2">
+                                <img src="{{ url('storage/' . $post->preview_image) }}" alt="" class="w-50">
+                            </div>
+                            <div class="input-group">
+                                <div class="custom-file">
+                                    <input type="file" class="custom-file-input" name="preview_image"  >
+                                    <label class="custom-file-label"  >Выберите изображение</label>
+                                </div>
+                                <div class="input-group-append">
+                                    <span class="input-group-text">Загрузка</span>
+                                </div>
+                            </div>
+                            @error('preview_image')
+                                <div class="text-danger">Загрузите превью</div>
+                            @enderror
+                        </div>
+                        <div class="form-group w-25">
+                            <label for="exampleInputFile">Добавить главное изображение</label>
+                            <div class="w-100 mb-2">
+                                <img src="{{ url('storage/' . $post->main_image ) }}" alt="" class="w-50">
+                            </div>
+                            <div class="input-group">
+                                <div class="custom-file">
+
+                                    <input type="file" class="custom-file-input" name="main_image" >
+                                    <label class="custom-file-label"  >Выберите изображение</label>
+                                </div>
+                                <div class="input-group-append">
+                                    <span class="input-group-text">Загрузка</span>
+                                </div>
+                            </div>
+                            @error('main_image')
+                            <div class="text-danger">Загрузите главное изображение</div>
+                            @enderror
+                        </div>
+                        <div class="form-group w-25">
+                            <label>Выбирите категорию</label>
+                            <select name="category_id" class="custom-select">
+                                @foreach($categories as $category)
+                                    <option value="{{ $category->id }}"
+                                        {{ $category->id == $post->category_id ? 'selected' : '' }}
+                                    >{{ $category->title }}</option>
+                                @endforeach
+                            </select>
+                            @error('category_id')
+                            <div class="text-danger">Выбирите категорию</div>
+                            @enderror
+                        </div>
+                        <div class="form-group">
+                            <label>Теги</label>
+                            <select class="select2" name="tag_ids[]" multiple="multiple" data-placeholder="Выбириет теги" style="width: 100%;">
+                                @foreach($tags as $tag)
+                                    <option value="{{ $tag->id }}"
+                                        {{ is_array($post->tags->pluck('id')->toArray()) && in_array($tag->id, $post->tags->pluck('id')->toArray()) ? ' selected' : ''}}
+                                    >{{ $tag->title }}</option>
+                                @endforeach
+                            </select>
                         </div>
                         <div class="form-group">
                             <input type="submit" class="btn btn-primary" value="Обновить">
